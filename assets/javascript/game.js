3484fc2c3b1e645f36f12c6df6608d446413c3a5
Rose, Simone <simoneidance@icloud.com>
2019-05-16 22:45:24 -0500

    Java Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,28 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+$(document).ready(function() {
+
+    var correctLetter = 
+    var incorrectLetter =
+    var guess =
+
+    if (correctLetter) {
+        
+    }
+
+    if (incorrectLetter) = {
+        
+    }
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+ 
+ console.log(makeid(i));
